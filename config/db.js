271eb55cb592dd7b6b85dacb38a2c0f70c93efa8
Sauhard <samsauhard1@gmail.com>
2019-02-04 16:35:50 -0500

    Changes to URI - MLAB

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-   "URI" : "mongodb://sauhard:9896882814@ds121105.mlab.com:21105/sauhard"
+   "URI" : "mongodb://sauhard:A123456@ds121105.mlab.com:21105/sauhard"
     //"URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson4b"
 }
